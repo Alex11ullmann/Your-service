0a2970ed2076c24408298d22e67ef70bda36be05
lucaslapla <lucaslapla@hotmail.com>
2025-04-26 11:20:46 -0300

    Se carga CSS y se ajusta JS, del cuerpo en Principal

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 let lista=document.getElementById("lista");
 
 const dirImagenes=  ["./ImgTrabj/juan.jfif", "./ImgTrabj/lucas.jfif",   "./ImgTrabj/alexis.jfif", "./ImgTrabj/pablo.jfif", "./ImgTrabj/pedro.jfif", "./ImgTrabj/maria.jfif",   "./ImgTrabj/jose.jfif",  "./ImgTrabj/mauricio.jfif",  "./ImgTrabj/agustina.jfif",  "./ImgTrabj/juana.jfif"];                                                                                                                      
-const nombre=       ["juan",        "lucas",          "alexis",        "pablo",                  "pedro",        "maria",          "jose",         "Mauricio",         "Agustin",          "juana"];
+const nombre=       ["Juan",        "Lucas",          "Alexis",        "Pablo",                  "Pedro",        "Maria",          "jose",         "Mauricio",         "Agustina",          "Juana"];
 const calificacion= ["⭐⭐",       "⭐⭐⭐",       "⭐⭐⭐⭐",    "⭐⭐⭐⭐⭐",          "⭐⭐",         "⭐⭐⭐",        "⭐⭐",        "⭐⭐⭐",          "⭐⭐",            "⭐⭐⭐"];
 const telefono    = [1111111,       22222222         ,3333333333       ,444444444444           ,55555555        ,666666666        ,7777777777     ,8888888888         ,999999999            ,100000000   ];
 
@@ -15,19 +15,18 @@ function crearLista (dirimagenes,arraynombre,arrayCalificacion,telefono){
         img.src=dirimagenes[i];
         img.width=170;
         img.height=170;
-        
-    
-         let button=document.createElement("button");
-         button.setAttribute("class","button");
-         button.textContent = "Contactar";
-         //contactar.addEventListener('click', contactarTrabajador(telefono[i])); //primero validamos, luego llamos funcion de compra;
-          
-        
+              
+        let button=document.createElement("button");
+        button.setAttribute("class","button");
+        button.textContent = "Contactar";
+        button.addEventListener('click', function() {
+                contactarTrabajador(telefono[i])
+            }); //llama la funcion para mostrar los telefonos
+                 
         li.appendChild(img);//asigno la imagen dentro de la lista.
         li.appendChild(text);//asigno el texto(Nombre, calificacion) a la lista
         li.appendChild(button);
-        lista.appendChild(li);//Creolista con imagen, texto .
-        
+        lista.appendChild(li);//Creolista con imagen, texto y el boton.
     }
 }
 
