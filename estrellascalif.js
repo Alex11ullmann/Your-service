924ab397f169f95f0af536b1fc55d23ca730d052
Alex <alex11ullmann@gmail.com>
2025-05-28 14:28:56 -0300

    subo modificacion de registro y login

---
+++ estrellascalif.js
@@ -0,0 +1,65 @@
+en html 
+/*
+<div class="segundaparte">
+
+            <div class="valoracion">
+                 radio se usa para que solo pueda seleccionar una estrella a la vez 
+                <input type="radio" id="estrella5" name="valoracion" value="5">
+                <label for="star5">★</label>
+                <input type="radio" id="estrella4" name="valoracion" value="4">
+                <label for="star4">★</label>
+                <input type="radio" id="estrella3" name="valoracion" value="3">
+                <label for="star3">★</label>
+                <input type="radio" id="estrella2" name="valoracion" value="2">
+                <label for="star2">★</label>
+                <input type="radio" id="estrella1" name="valoracion" value="1">
+                <label for="star1">★</label>
+            </div>
+
+            <div class="valor">
+
+                <a> 5.0 </a>
+            </div>
+
+        </div>
+*/
+en style
+/*
+.valor {
+    display: flex;
+    margin-top: 8px;
+    font-size: 3vw;
+    color:gray;
+    font-family: 'Calibri Light', Calibri, sans-serif;
+    border-color: black;
+    text-shadow: 
+        1px 1px 0px black,
+        -1px -1px 0px black,
+        1px -1px 0px black,
+        -1px 1px 0px black;
+    text-decoration: none;
+    flex-direction: row; 
+    justify-content: center;
+}
+
+.valoracion {
+    display: flex;
+    flex-direction: /*row-reverse; revierte el orden de las estrellas
+    justify-content: center;
+}
+.valoracion input {
+    display: none; /*Oculta los botones de radio
+}
+.valoracion label {
+    font-size: 4vw;
+    color: gray;
+    cursor: pointer; /*Convierte el cursor
+}
+.valoracion input:checked ~ label {
+    color: gold; /*Cambia el color del imput seleccionado y sus anteriores
+}
+.valoracion label:hover, .valoracion label:hover ~ label {
+    color: gold; /*Al pasar el mouse sobre una estrella, esa y las anteriores cambian de color
+}
+*/
+
