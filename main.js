7d9b1eb86d33cf9f01c990a2bf923fbb89ea9fcb
lucaslapla <lucaslapla@hotmail.com>
2025-06-12 15:36:39 -0300

    Se crea carpeta DATA y Profecionales.json Cliente.json

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 let lista=document.getElementById("lista");
 
+/*
 const dirImagenes=  ["./ImgTrabj/juan.jfif", "./ImgTrabj/lucas.jfif",   "./ImgTrabj/alexis.jfif", "./ImgTrabj/pablo.jfif", "./ImgTrabj/pedro.jfif", "./ImgTrabj/maria.jfif",   "./ImgTrabj/jose.jfif",  "./ImgTrabj/mauricio.jfif",  "./ImgTrabj/agustina.jfif",  "./ImgTrabj/juana.jfif"];                                                                                                                      
 const nombre=       ["Juan",        "Lucas",          "Alexis",        "Pablo",                  "Pedro",        "Maria",          "jose",         "Mauricio",         "Agustina",          "Juana"];
 const calificacion= ["⭐⭐",       "⭐⭐⭐",       "⭐⭐⭐⭐",    "⭐⭐⭐⭐⭐",          "⭐⭐",         "⭐⭐⭐",        "⭐⭐",        "⭐⭐⭐",          "⭐⭐",            "⭐⭐⭐"];
@@ -46,3 +47,60 @@ function mostrarPerfilTrabajador(imagen,nombre,calificacion,telefono){
 
 crearLista(dirImagenes,nombre,calificacion,telefono);
 
+*/
+
+
+function crearListaDesdeJSON(data) {
+  data.forEach(persona => {
+    let li = document.createElement("li");
+
+    const img = document.createElement("img");
+    img.src = persona.imagen;
+    img.style.width = "17vw";
+    img.style.height = "17vw";
+
+    let text = document.createElement("p");
+    text.textContent = `${persona.nombre} (${persona.Oficio}): ${persona.calificacion}`;
+    text.style.fontSize = "1.5vw";
+
+    let button = document.createElement("button");
+    button.setAttribute("class", "button");
+    button.textContent = "Contactar";
+    button.addEventListener("click", () => {
+        mostrarPerfilTrabajador(
+              persona.imagen,
+              persona.nombre,
+              persona.calificacion,
+              persona.telefono,
+              persona.localidad,
+              persona.direccion,
+              persona.dni,
+              persona.Oficio
+            );
+    });
+
+    li.appendChild(img);
+    li.appendChild(text);
+    li.appendChild(button);
+    lista.appendChild(li);
+  });
+}
+
+function mostrarPerfilTrabajador(imagen, nombre, calificacion, telefono, localidad, direccion, dni, oficio) {
+  alert(`Nombre: ${nombre}
+        Oficio: ${oficio}
+        Calificación: ${calificacion}
+        Teléfono: ${telefono}
+        Localidad: ${localidad}
+        Dirección: ${direccion}
+        DNI: ${dni}`);
+}
+
+fetch("./Data/profecionales.json")
+  .then(response => response.json())
+  .then(data => {
+    crearListaDesdeJSON(data);
+  })
+  .catch(error => console.error("Error al cargar JSON:", error));
+
+  
