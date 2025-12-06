1699e9d4f6acd4c6a2aa910a143dee4bfada6675
Alex <alex11ullmann@gmail.com>
2025-12-06 16:38:32 -0300

    solucionando registro usuario normal FE

--- nest-ys/src/perfil/dto/update-perfil.dto.ts
+++ nest-ys/src/perfil/dto/update-perfil.dto.ts
@@ -1,5 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePerfilDto } from './create-perfil.dto';
+import { IsOptional, IsString, Length } from 'class-validator';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-export class UpdatePerfilDto extends PartialType(CreatePerfilDto) {}
+export class UpdatePerfilDto extends PartialType(CreatePerfilDto) {
+    @IsString()
+    @IsOptional()
+    @Length(0, 600, { message: "El campo Sobre Mi debe tener como máximo 600 caracteres" })
+    descripcion?: string;
+}
